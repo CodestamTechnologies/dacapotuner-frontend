a8646826b18c4248b793af0faf1f311068649c41
souravvmishra <souravvmishra@gmail.com>
2023-11-05 23:32:39 +0530

    redesigned

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
+import Footer from '@/components/Footer'
 import Homepage from '@/components/Homepage'
-import NavBar from '@/components/Navbar'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='bg-black'>
+    <main>
       <Homepage />
+      <Footer />
     </main>
   )
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='text-center font-medium font-sans py-4 border-t '>
+Copyright ©️ 2023 Dacapo | All Rights Reserved 
+            </div>
+        </div>
+    )
+}
+
+export default Footer
